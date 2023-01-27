530871ff936fa510bfcdd6b860c864d1753778cd
buddy <jang_taehwan@naver.com>
2023-01-27 19:03:09 +0900

    git init

---
+++ src/main/java/org/study/oauth/auth/controller/AuthController.java
@@ -0,0 +1,11 @@
+package org.study.oauth.auth.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/auth")
+@RestController
+public class AuthController {
+
+}
